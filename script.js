b8aa724098b88f3c7d135d4503e36990af0e4f8b
Gleb Marenin <glebmarenin20@gmail.com>
2022-12-22 13:24:53 +0300

    counter

---
+++ script.js
@@ -0,0 +1,33 @@
+const actions = Array.from(document.querySelectorAll('[data-action]'));
+console.log(actions);
+
+// if counter !== 0, counter == counter's saved value
+var counter =  localStorage.getItem('counter') ||0;
+document.querySelector('.num').innerText=counter;
+
+actions.forEach((action)=>{
+    action.addEventListener('click', ()=>{
+        const type = action.dataset.action;
+        
+        switch (type) {
+            case 'reduce': 
+            counter--;
+                break
+            case 'reset':
+            counter = 0;
+                break
+            case 'add':
+            counter++;
+                break
+            case 'save':
+                //to save counter's value in localStorage
+            localStorage.setItem('counter', counter);
+                break
+            case 'clear':
+            localStorage.clear();
+            counter = 0;
+            break
+        }
+        document.querySelector('.num').innerText= counter;
+    })
+})
