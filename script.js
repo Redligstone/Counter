d5f502c7b89c4052cfa75c346888f8547ed0f502
Gleb Marenin <glebmarenin20@gmail.com>
2023-12-24 15:24:07 +0300

    new-counter

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
-const actions = Array.from(document.querySelectorAll('[data-action]'));
+const actions = Array.from(document.querySelectorAll("[data-action]"));
 console.log(actions);
+// hei, i am a new counter
 
 // if counter !== 0, counter == counter's saved value
-var counter =  localStorage.getItem('counter') ||0;
-document.querySelector('.num').innerText=counter;
+var counter = localStorage.getItem("counter") || 0;
+document.querySelector(".num").innerText = counter;
 
-actions.forEach((action)=>{
-    action.addEventListener('click', ()=>{
-        const type = action.dataset.action;
-        
-        switch (type) {
-            case 'reduce': 
-            counter--;
-                break
-            case 'reset':
-            counter = 0;
-                break
-            case 'add':
-            counter++;
-                break
-            case 'save':
-                //to save counter's value in localStorage
-            localStorage.setItem('counter', counter);
-                break
-            case 'clear':
-            localStorage.clear();
-            counter = 0;
-            break
-        }
-        document.querySelector('.num').innerText= counter;
-    })
-})
+actions.forEach((action) => {
+  action.addEventListener("click", () => {
+    const type = action.dataset.action;
+
+    switch (type) {
+      case "reduce":
+        counter--;
+        break;
+      case "reset":
+        counter = 0;
+        break;
+      case "add":
+        counter++;
+        break;
+      case "save":
+        //to save counter's value in localStorage
+        localStorage.setItem("counter", counter);
+        break;
+      case "clear":
+        localStorage.clear();
+        counter = 0;
+        break;
+    }
+    document.querySelector(".num").innerText = counter;
+  });
+});
